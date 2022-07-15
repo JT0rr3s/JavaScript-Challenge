ae9ac8b5e60bacaceafad02b29b295aba95872aa
Josh Torres <jtorres5392@gmail.com>
2022-07-14 23:59:21 -0500

    Function written to convert Days to Age

--- Challenge.js
+++ Challenge.js
@@ -1,11 +1,43 @@
+// 1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+
 const isNumberPositive = (trueOrFalse) => {
    if (trueOrFalse >= 0) {
-    console.log(true)
+    console.log(true);
    }
    else {
-    console.log(false)
+    console.log(false);
    }
 }
 
-isNumberPositive(-1)
-isNumberPositive(10)
+isNumberPositive(-1) //returns false
+isNumberPositive(10) //returns true
+
+// 2. Write a function that takes a number of days and converts it into an age.
+
+const convertDaysToAge = (numberOfDays) => {
+    Age = numberOfDays / 365;
+    console.log(Age);
+}
+
+convertDaysToAge(3650); // returns 10
+convertDaysToAge(6570); // returns 18
+
+// 3. Write a function that takes three numbers and returns the largest of the three numbers.
+
+
+//getLargestNumber(2 ,1, 4); // returns 4
+//getLargestNumber(6,2,3); // returns 6
+// 4. Write a function that takes an array of names and returns the last name from the array of names.
+
+
+
+//getLastName([”Charlie”, “Rob”, “Andy”]); // returns “Andy”
+//getLastName(["Ash","Stu"]); // returns "Stu"
+
+
+// 5. Write a function that takes an array of numbers and returns true if all of the numbers are positive.
+//    It should return false if there are one or more negative numbers in the array.
+
+
+//allNumbersPositive([2,4,5]); // returns true
+//allNumbersPositive([-5,4,6]); // returns false
