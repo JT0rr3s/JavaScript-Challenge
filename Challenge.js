57fbc4197f541b38ff92f25df0c4ced08fae61a9
Josh Torres <jtorres5392@gmail.com>
2022-07-14 23:42:30 -0500

    True/False function written to determine if a number is positive or negative

---
+++ Challenge.js
@@ -0,0 +1,11 @@
+const isNumberPositive = (trueOrFalse) => {
+   if (trueOrFalse >= 0) {
+    console.log(true)
+   }
+   else {
+    console.log(false)
+   }
+}
+
+isNumberPositive(-1)
+isNumberPositive(10)
